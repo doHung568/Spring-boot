90e2a37bb973ced241c15559c786dee8ce310290
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-11-24 14:14:42 +0700

    basic of RequestParam

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,8 +1,16 @@
 package com.javaweb.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BuildingAPI {
-
+	@RequestMapping(value = "/api/building", method = RequestMethod.GET)
+	public void getBuilding(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "floor", required = false) String floor,
+			@RequestParam(value = "price", required = false) Integer price) {
+		System.out.println(name + " " + floor + " " + price);
+	}
 }
