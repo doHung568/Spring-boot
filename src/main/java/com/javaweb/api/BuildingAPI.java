daafc160f8e53bc54797f34dc67c1a078188512e
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-11-24 16:16:05 +0700

    upgrade annotation for Controller and RequestMapping

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -3,25 +3,22 @@ package com.javaweb.api;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaweb.beans.BuildingDTO;
 
-@Controller
+@RestController
 public class BuildingAPI {
-	@ResponseBody
-	@RequestMapping(value = "/api/building", method = RequestMethod.GET)
+	@GetMapping("/api/building")
 	public List<BuildingDTO> getBuilding(@RequestBody List<BuildingDTO> buildingDTO) {
 		List<BuildingDTO> result = new ArrayList<BuildingDTO>();
 
 		// handle logic
 		for (BuildingDTO buildingDTO2 : buildingDTO) {
 			if (buildingDTO2.getPrice() < 100000) {
-				buildingDTO2.setPrice(88888);
+				buildingDTO2.setPrice(12388888);
 			}
 		}
 
