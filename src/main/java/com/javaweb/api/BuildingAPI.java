46c44f4728c44019b55ffdb2e4c96b4bb1bddfa9
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-11-25 03:54:06 +0700

    use @PathVariable

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,32 +1,23 @@
 package com.javaweb.api;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.javaweb.beans.BuildingDTO;
 
 @RestController
 public class BuildingAPI {
-	@GetMapping("/api/building")
-	public List<BuildingDTO> getBuilding(@RequestBody List<BuildingDTO> buildingDTO) {
-		List<BuildingDTO> result = new ArrayList<BuildingDTO>();
-
-		// handle logic
-		for (BuildingDTO buildingDTO2 : buildingDTO) {
-			if (buildingDTO2.getPrice() < 100000) {
-				buildingDTO2.setPrice(12388888);
-			}
+	@PostMapping({ "/api/building/{id}", "/api/building/{id}/{name}" })
+	public String updateBuilding(@PathVariable(required = false) String id, @PathVariable(required = false) String name,
+			@RequestParam(value = "xM", required = false) String xM) {
+		if (id == null) {
+			id = "Missing id";
 		}
 
-		// add data in result
-		for (BuildingDTO buildingDTO2 : buildingDTO) {
-			result.add(buildingDTO2);
+		if (name == null) {
+			name = "Missing name";
 		}
 
-		return result;
+		return id + " " + name + " " + xM;
 	}
 }
