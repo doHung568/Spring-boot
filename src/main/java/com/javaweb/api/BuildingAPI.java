9a91fb5881433b5c66aaa5c11ed567d8298b0089
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-11-24 16:01:43 +0700

    use basic of RequestBody

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,16 +1,35 @@
 package com.javaweb.api;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.javaweb.beans.BuildingDTO;
 
 @Controller
 public class BuildingAPI {
+	@ResponseBody
 	@RequestMapping(value = "/api/building", method = RequestMethod.GET)
-	public void getBuilding(@RequestParam Map<String, String> map) {
-		System.out.println("ok");
+	public List<BuildingDTO> getBuilding(@RequestBody List<BuildingDTO> buildingDTO) {
+		List<BuildingDTO> result = new ArrayList<BuildingDTO>();
+
+		// handle logic
+		for (BuildingDTO buildingDTO2 : buildingDTO) {
+			if (buildingDTO2.getPrice() < 100000) {
+				buildingDTO2.setPrice(88888);
+			}
+		}
+
+		// add data in result
+		for (BuildingDTO buildingDTO2 : buildingDTO) {
+			result.add(buildingDTO2);
+		}
+
+		return result;
 	}
 }
