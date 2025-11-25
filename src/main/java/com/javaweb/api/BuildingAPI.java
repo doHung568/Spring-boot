bb2db58ea15c5ae380fe2aa05c8c1cf20719977a
doHung568 <contact.me.dothehung@gmail.com>
2025-11-26 00:54:00 +0700

    use controller advice to handle logic

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,14 +1,10 @@
 package com.javaweb.api;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaweb.beans.BuildingDTO;
-import com.javaweb.beans.ErrorResponseDTO;
 
 import customException.FieldRequiredException;
 
@@ -17,22 +13,8 @@ public class BuildingAPI {
 	@GetMapping("/test-custom-exception")
 	// return Object
 	public Object getBuilding(@RequestBody BuildingDTO buildingDTO) {
-
 		// validate buildingDTO
-		try {
-			isNullOrEmpty(buildingDTO);
-		} catch (FieldRequiredException e) {
-			ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
-			errorResponseDTO.setNameOfError(e.getMessage());
-			List<String> detailsList = new ArrayList<>();
-			// add details for exception
-			detailsList.add("Requird re-check input name");
-			detailsList.add("Requird re-check input floor");
-			detailsList.add("Requird re-check input price");
-			errorResponseDTO.setDetails(detailsList);
-			// return error
-			return errorResponseDTO;
-		}
+		isNullOrEmpty(buildingDTO);
 		// else return Success
 		return "Success";
 	}
@@ -42,7 +24,15 @@ public class BuildingAPI {
 		if (buildingDTO.getName() == null || buildingDTO.getName().equals("") || buildingDTO.getFloor() == null
 				|| buildingDTO.getPrice() == null) {
 			// return name of exception
+			// when throw exception -> Spring MVC scan @ExceptionHanlder
 			throw new FieldRequiredException("Input can not be empty");
 		}
+	}
+
+	@GetMapping("/test-controllerAdvice")
+	public void test() {
+
+		System.out.println(5 / 0);
+
 	}
 }
