e8037390228e36243e4e88e12c8243ad59656daf
doHung568 <contact.me.dothehung@gmail.com>
2025-11-25 14:41:32 +0700

    use custom exception

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,23 +1,38 @@
 package com.javaweb.api;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.beans.BuildingDTO;
+import com.javaweb.beans.ErrorResponseDTO;
 
 @RestController
 public class BuildingAPI {
-	@PostMapping({ "/api/building/{id}", "/api/building/{id}/{name}" })
-	public String updateBuilding(@PathVariable(required = false) String id, @PathVariable(required = false) String name,
-			@RequestParam(value = "xM", required = false) String xM) {
-		if (id == null) {
-			id = "Missing id";
+	@GetMapping("/test-error")
+	// return Object
+	public Object getBuilding() {
+		BuildingDTO buildingDTO = new BuildingDTO();
+
+		ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+		try {
+			// error
+			System.out.println(5 / 0);
+		} catch (Exception e) {
+			errorResponseDTO.setNameOfError(e.getMessage());
+			List<String> detailsList = new ArrayList<>();
+			detailsList.add("Error divide by 0");
+			detailsList.add("Error input < 10");
+			errorResponseDTO.setDetails(detailsList);
+			// return error
+			return errorResponseDTO;
 		}
 
-		if (name == null) {
-			name = "Missing name";
-		}
-
-		return id + " " + name + " " + xM;
+		buildingDTO.setName("Vinhome");
+		buildingDTO.setFloor(10);
+		// return Object BuildingDTO
+		return buildingDTO;
 	}
 }
