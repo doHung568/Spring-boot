16d6929380345254cf3fc05e86daacfb5479f079
doHung568 <contact.me.dothehung@gmail.com>
2025-11-25 16:43:32 +0700

    create package and class to custom exception

---
+++ src/main/java/customException/FieldRequiredException.java
@@ -0,0 +1,14 @@
+package customException;
+
+public class FieldRequiredException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FieldRequiredException(String message) {
+		super(message);
+	}
+
+}
